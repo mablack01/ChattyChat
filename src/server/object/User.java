c85844d9bc4a89e52607f41f9311a88cbcb74aa4
DESKTOP-78K1091\Hi Im Paul <mikulskisp@gmail.com>
2017-12-23 23:44:38 -0500

    Some bumdiddlees for server thingies

---
+++ src/server/object/User.java
@@ -0,0 +1,49 @@
+package server.object;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Enumeration;
+import static java.lang.System.out;
+
+public class User {
+
+	private String userName;
+	private String password;
+	private String ip;
+	
+	//Constructor
+	public User(String u, String p) throws SocketException{
+		userName = u;
+		password = p;
+		Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
+		
+		for (NetworkInterface netint : Collections.list(n)){
+			if(netint.getDisplayName().contains("Wireless".toString())){
+				Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+				for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+		            if(inetAddress.toString().length() > 0){
+		            String ip = inetAddress.toString();
+		            ip = ip.substring(1);
+		            this.ip = ip;
+					out.printf("New user created with ip: %s\n", this.ip);
+		            }
+		        }
+			}
+		}
+	}
+
+	//Getters
+	public String getUserName(){
+		return this.userName;
+	}
+	public String getPassword(){
+		return this.password;
+	}
+	public String getIP(){
+		return this.ip;
+	}
+}
+	
+
